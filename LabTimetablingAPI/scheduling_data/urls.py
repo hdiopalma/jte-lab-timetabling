2be741a7eccdba8a83f1571aefca81b798d9435e
Hernando Dio Palma <hernando.diopalma@gmail.com>
2023-07-12 23:18:16 +0700

    Insert mock data and update URL config for participant list

--- LabTimetablingAPI/scheduling_data/urls.py
+++ LabTimetablingAPI/scheduling_data/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('schedule/', views.schedule_practicum, name="schedule")
+    path('schedule/', views.schedule_practicum, name="schedule"),
+    path('participant/', views.participant_list, name="participant_list"),
 ]
--- LabTimetablingAPI/scheduling_data/views.py
+++ LabTimetablingAPI/scheduling_data/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from scheduling_data.models import Semester, Laboratory, Module, Chapter, Group, Participant, Assistant
+
 # Create your views here.
 
 @csrf_exempt
@@ -10,3 +12,11 @@ def schedule_practicum(request):
         return JsonResponse({"message": "Hello, world!"})
     else:
         return JsonResponse({"error": "Invalid request method"}, status=405)
+
+@csrf_exempt
+def participant_list(request):
+    if request.method == "GET":
+        participants = Participant.objects.all()
+        data = [{'name' : participant.name,'nim' : participant.nim, 'semester' : participant.semester.name, 'regular_schedule': participant.regular_schedule} for participant in participants]
+        
+        return JsonResponse(data, safe=False)
