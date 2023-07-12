3c1d3108de435ab808d5cd11dbcc0f447a3d9638
Hernando Dio Palma <hernando.diopalma@gmail.com>
2023-07-12 23:17:26 +0700

    Insert mockup data and update URC configuration for participant list.

--- LabTimetablingAPI/scheduling_data/models.py
+++ LabTimetablingAPI/scheduling_data/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Semester(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=24)
     
     def __str__(self) -> str:
         return self.name
@@ -14,37 +14,7 @@ class Laboratory(models.Model):
     
     def __str__(self) -> str:
         return self.name
-
-class Group(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=10)
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
     
-    def __str__(self) -> str:
-        return self.name
-
-class Participant(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    nim = models.CharField(max_length=12)
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
-    regular_schedule = models.JSONField()
-    
-    def __str__(self) -> str:
-        return self.name
-
-class Assistant(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    nim = models.CharField(max_length=12)
-    laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)
-    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
-    regular_schedule = models.JSONField()
-    prefered_schedule = models.JSONField()
-    
-    def __str__(self) -> str:
-        return self.name
-
 class Module(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32)
@@ -59,6 +29,37 @@ class Chapter(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=32)
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Group(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Participant(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    nim = models.CharField(max_length=12)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    regular_schedule = models.JSONField() 
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Assistant(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    nim = models.CharField(max_length=12)
+    laboratory = models.ForeignKey(Laboratory, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semester, on_delete=models.CASCADE)
+    regular_schedule = models.JSONField()
+    prefered_schedule = models.JSONField()
     
     def __str__(self) -> str:
         return self.name
